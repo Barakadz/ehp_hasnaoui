5d4f92003d483dd88635f84aa0751ed08a87180f
BarakaDz <ibrahimabbesbaraka@mail.com>
2024-05-05 06:53:05 +0200

    cc

--- client/components/rendez/rendez.jsx
+++ client/components/rendez/rendez.jsx
@@ -15,9 +15,47 @@ const Rendezvous=()=>{
     };
     
     const options = [
-    { value: '1', label: 'Option 1' },
-    { value: '2', label: 'Option 2' },
-    { value: '3', label: 'Option 3' },
+    { value: 'Consultation médecine générale', label: 'Consultation médecine générale' },
+    { value: 'gynéco-obstétrique', label: 'gynéco-obstétrique' },
+    { value: 'pédiatrie', label: 'pédiatrie' },
+	{ value: 'cardiologie', label: 'cardiologie' },
+    { value: 'dermatologie', label: 'dermatologie' },
+    { value: 'endocrinologie', label: 'endocrinologie' },
+    { value: 'traumatologie', label: 'traumatologie' },
+    { value: 'anesthésie', label: 'anesthésie' },
+    { value: 'chirurgie générale', label: 'chirurgie générale' },
+    { value: 'chirurgie cardio-vasculaire', label: 'chirurgie cardio-vasculaire' },
+    { value: 'Laboratoire d’analyses médicales', label: 'Laboratoire d’analyses médicales' },
+    { value: 'IRM', label: 'IRM' },
+    { value: 'Scanner', label: 'Scanner' },
+    { value: 'Mammographie', label: 'Mammographie' },
+    { value: 'Radiographie Standard', label: 'Radiographie Standard' },
+    { value: 'Télémétrie', label: 'Télémétrie' },
+    { value: 'Panoramique dentaire', label: 'Panoramique dentaire' },
+    { value: 'DMO', label: 'DMO' },
+    { value: 'Urgences (médicales)', label: 'Urgences (médicales)' },
+    { value: 'Urgences (chirurgicales)' , label: 'Urgences (chirurgicales)' },
+    { value: 'Réadaptation fonctionnelle', label: 'Réadaptation fonctionnelle' },
+    { value: 'gynécologie', label: 'gynécologie' },
+    { value: 'obstétrique', label: 'obstétrique' },
+    { value: 'pédiatrie', label: 'pédiatrie' },
+    { value: 'néonatologie', label: 'néonatologie' },
+    { value: 'viscérale', label: 'viscérale' },
+    { value: 'cardio-vasculaire,', label: 'cardio-vasculaire,' },
+    { value: 'cardiologie-interventionnelle', label: 'cardiologie-interventionnelle' },
+    { value: 'vasculaire', label: 'vasculaire' },
+    { value: 'neurochirurgie', label: 'neurochirurgie' },
+    { value: 'gynéco-obstétrique', label: 'gynéco-obstétrique' },
+	{ value: 'ophtalmologie', label: 'ophtalmologie' },
+    { value: 'orthopédique et traumatologique', label: 'orthopédique et traumatologique' },
+    { value: 'explorations endoscopiques', label: 'explorations endoscopiques' },
+    { value: 'stérilisation', label: 'stérilisation' },
+    { value: 'transport sanitaire', label: 'transport sanitaire' },
+    { value: 'buanderie', label: 'buanderie' },
+	{ value: 'restauration', label: 'restauration' },
+
+
+
   ];
     return(
         <div>
@@ -26,9 +64,11 @@ const Rendezvous=()=>{
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="section-title text-center">
-							<h2>We Are Always Ready to Help You. Book An Appointment</h2>
+							<h2>Prendre un Rendez-vous
+</h2>
 							<img src="section-img.png" alt="#"/>
-							<p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p>
+							<p>
+Prendre un rendez-vous médical en ligne permet de choisir facilement et rapidement le jour et l’heure de la consultation,<br/> sans avoir à se déplacer. Ce service est gratuit, convivial et sécuritaire.</p>
 						</div>
 					</div>
 				</div>
@@ -38,7 +78,29 @@ const Rendezvous=()=>{
 							<div class="row">
 								<div class="col-lg-6 col-md-6 col-12">
 									<div class="form-group">
-										<input name="name" type="text" placeholder="Name"/>
+										<input name="name" type="text" placeholder="Nom *"/>
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 col-12">
+									<div class="form-group">
+										<input name="name" type="text" placeholder="Prénom *"/>
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 col-12">
+									<div class="form-group">
+ 										<DatePicker
+        selected={selectedDate}
+        onChange={handleChange}
+        dateFormat="dd/MM/yyyy"
+
+         
+        placeholderText="Dat de naissance"
+      />						
+									</div>
+								</div>
+								<div class="col-lg-6 col-md-6 col-12">
+									<div class="form-group">
+										<input name="name" type="text" placeholder="Prénom"/>
 									</div>
 								</div>
 								<div class="col-lg-6 col-md-6 col-12">
