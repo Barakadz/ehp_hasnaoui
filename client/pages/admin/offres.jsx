20882f942d38e4a3ab5a530419949287fb44588e
baraka <ibrahimabbesbaraka@gmail.com>
2024-05-13 16:54:07 +0100

    sdsd

---
+++ client/pages/admin/offres.jsx
@@ -0,0 +1,24 @@
+
+
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+ 
+
+export default function Offres() {
+ 
+  return (
+  <>
+	<Head>
+	<title>Actualités - EHPHASNAOUI</title>
+	<meta name="description" content="Ehp hasnaoui, Hopital Privée Hasnaui,Hasnaoui Private Hospital, Ehp-hasnaoui" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="icon" href="/logozoom.PNG" />
+  </Head>
+  <main style={{padding:'0'}}>
+ 
+ <p>fff</p>
+     </main>
+</>
+  );
+}
