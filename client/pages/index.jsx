adb21e6d5308241e18a8ad859c4ee17be8427030
baraka <ibrahimabbesbaraka@gmail.com>
2024-05-22 10:21:57 +0100

    sssffsf

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -12,12 +12,16 @@ import React, { useEffect } from 'react';
 import HeroSlider from "@/components/slide/slide";
 import Convention from "@/components/convention/convention";
 import Actualites from "@/components/actualites/actualites";
- 
-   
+  
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+ 
+
+  
   
   useEffect(() => {
     const handleLoad = () => {
@@ -55,10 +59,12 @@ export default function Home() {
 <HeroSlider/>
 <EhphWhy/>
  <Video/>
- <Convention/><center>
+ <Convention/>
+ 
+ <center>
 				<div class="row">
 					<div class="col-lg-12">
-						<div class="section-title">
+						<div class="section-title wow fadeIn" data-wow-delay="0.5s">
 							<h2>Suivez nos dernières Actualités :</h2>
 							<img src="section-img.png" className="mb-3" alt="#"/>
  						</div>
