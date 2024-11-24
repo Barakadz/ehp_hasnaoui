82bfa65420a602b82d933ad9254eb5fce17db0f1
BarakaDz <ibrahimabbesbaraka@mail.com>
2024-11-24 13:50:42 +0100

    jjjjjjjj

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,6 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  apps: [
+    {
+      name: 'frontend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        PORT: 3001,
+      },
+    },
+  ],
   output: 'export',
   reactStrictMode: true,
   env: {
